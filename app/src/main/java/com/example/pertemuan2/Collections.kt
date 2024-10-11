b1621d98eecaed15c5536641b10501d3c04d68da
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-10-11 18:07:06 +0700

    project created

---
+++ app/src/main/java/com/example/pertemuan2/Collections.kt
@@ -0,0 +1,29 @@
+package com.example.pertemuan2
+
+fun ContohList(){
+    println("=== LIST ===")
+    // List Read Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    print(shape)
+
+    // Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data dari List Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+    // Mengubah data di dalam List Mutable
+    shape[0] = "Oval"
+
+    // List Read Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+
+
+}
---
+++ app/src/main/java/com/example/pertemuan2/Variabel.kt
@@ -0,0 +1,41 @@
+package com.example.pertemuan2
+
+fun main () {
+
+    // BYTE
+    val byteValue: Byte = 10
+    println("Byte: $byteValue")
+
+    // INT
+    val intValue: Int = 30
+    println("Int: $intValue")
+
+    val IntValue: Int = 30
+    println(IntValue)
+
+    // FLOAT
+    val floatValue: Float = 50.6f
+    println("Float: $floatValue")
+
+    // DOUBLE
+    val doubleValue: Double = 60.6
+    println("Double: $doubleValue")
+
+    // CHAR
+    val charValue: Char = 'A'
+    println("Char: $charValue")
+
+    // BOLEAN
+    val booleanValue: Boolean = true
+    println("Boolean: $booleanValue")
+
+    // STRING
+    var stringValue: String = "Hello, World!"
+    println("String: $stringValue")
+
+    stringValue = "Hello, Kotlin!"
+    println("string: $stringValue")
+}
+
+
+
