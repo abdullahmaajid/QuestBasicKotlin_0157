2715b06e4b6dc36452ef051f457fab376112dabb
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-10-11 20:16:53 +0700

    Membuat funConditional Statement

---
+++ app/src/main/java/com/example/pertemuan2/ConditionalExpression.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan2
+
+// Conditional Expression
+// Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement(){
+    println("=== Conditional Expression ===")
+
+}
