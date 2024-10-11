a96722ef5cd61146d7e81cc372cae0d0be69eebc
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-10-11 20:12:13 +0700

    Membuat fun main untuk email

--- app/src/main/java/com/example/pertemuan2/Class.kt
+++ app/src/main/java/com/example/pertemuan2/Class.kt
@@ -3,3 +3,11 @@ package com.example.pertemuan2
 class Motor()
 
 class Contact(val id: Int, var email: String)
+
+fun main() {
+    val contact = Contact(1, "Mary@gmail.com")
+}
+
+// Prints the value of the property: email
+println(contact.email)
+
