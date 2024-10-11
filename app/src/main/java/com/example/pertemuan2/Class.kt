6de260ddee37093e9b135575e5ad4b8e9286fbbd
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2024-10-11 20:14:21 +0700

    Menambahkan Update Email

--- app/src/main/java/com/example/pertemuan2/Class.kt
+++ app/src/main/java/com/example/pertemuan2/Class.kt
@@ -6,8 +6,14 @@ class Contact(val id: Int, var email: String)
 
 fun main() {
     val contact = Contact(1, "Mary@gmail.com")
-}
+
 
 // Prints the value of the property: email
 println(contact.email)
 
+// Updates the value of the property: email
+contact.email = "jane@gmail.com"
+
+println(contact.email)
+
+}
